7f4fcb2fe924ee781965318edfbaa48676b0161c
Rohan Bharatia <167599567+Rohan-Bharatia@users.noreply.github.com>
2024-07-10 16:53:30 -0400

    Create toolchain.cmake

---
+++ toolchain.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_AR arm-none-eabi-ar)
+set(CMAKE_LINKER arm-none-eabi-ld)
+set(CMAKE_C_FLAGS "-mcpu=cortex-m3 -mthumb")
+set(CMAKE_CXX_FLAGS "-mcpu=cortex-m3 -mthumb")
+set(CMAKE_MAKE_PROGRAM make)
